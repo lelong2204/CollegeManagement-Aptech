a8a44d0c415ff2f115e3497a390933abeb955b0c
Le Thanh Long <lelong2242k@gmail.com>
2021-04-20 17:51:38 +0700

    ### add model

---
+++ CollegeManagement/Models/FacultySubject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeManagement.Models
+{
+    public class FacultySubject
+    {
+        public int ID { get; set; }
+        [ForeignKey("Faculty")]
+        public int FacultyID { get; set; }
+        [ForeignKey("Subject")]
+        public int SubjectID { get; set; }
+    }
+}
