50aad22dcce3926740d3f2be0bbf40b67c4eb573
lelong2204 <lelong2242k@gmail.com>
2021-05-12 22:47:12 +0700

    ### Update code

---
+++ CollegeManagement/DTO/Home/CourseHome.cs
@@ -0,0 +1,22 @@
+﻿using CollegeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeManagement.DTO.Home
+{
+    public class CourseHome
+    {
+        public string? Name { get; set; }
+        public string? Info { get; set; }
+        public int? DepartmentID { get; set; }
+        public int? StudentNumber { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int StudentRegister { get; set; }
+        public string? ImageURL { get; set; }
+        public int TotalBook { get; set; }
+        public int? Status { get; set; }
+    }
+}
